a85d84ad1a247fda66e7ecbe233140bd7f6e0ac9
Martijn Dirkse <martijn@integrationpartners.nl>
2023-06-21 12:19:36 +0200

    fix: write BuildInfo.properties in time

--- .github/workflows/writeBuildInfo.sh
+++ .github/workflows/writeBuildInfo.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "version=$1" > classes/BuildInfo.properties
-echo "versionDate_ddmmyyyy=$(date +%d/%m/%Y)" >> classes/BuildInfo.properties
+if [[ -n $1 ]]; then
+  echo "writeBuildInfo.sh - writing version $1"
+  echo "version=$1" > classes/BuildInfo.properties
+  echo "versionDate_ddmmyyyy=$(date +%d/%m/%Y)" >> classes/BuildInfo.properties
+else
+  echo "writeBuildInfo.sh - no version to write, leaving BuildInfo.properties unchanged"
+fi
