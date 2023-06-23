a16f2fa14d59dbccffcd3f32d37c1c82c90d8e1a
Martijn Dirkse <martijn@integrationpartners.nl>
2023-06-23 14:00:11 +0200

    fix: show version in console

--- .github/workflows/writeBuildInfo.sh
+++ .github/workflows/writeBuildInfo.sh
@@ -2,7 +2,7 @@
 
 if [[ -n $1 ]]; then
   echo "writeBuildInfo.sh - writing version $1"
-  echo "version=$1" > classes/BuildInfo.properties
+  echo "instance.version=$1" > classes/BuildInfo.properties
   echo "versionDate_ddmmyyyy=$(date +%d/%m/%Y)" >> classes/BuildInfo.properties
 else
   echo "writeBuildInfo.sh - no version to write, leaving BuildInfo.properties unchanged"
