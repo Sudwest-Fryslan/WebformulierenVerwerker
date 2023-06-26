b3ffa2978917a7b4bfc44aca1e8e66981cfdd610
Martijn Dirkse <martijn@integrationpartners.nl>
2023-06-26 15:09:12 +0200

    feat: make release for upload in hit

--- .github/workflows/writeBuildInfo.sh
+++ .github/workflows/writeBuildInfo.sh
@@ -4,6 +4,8 @@ if [[ -n $1 ]]; then
   echo "writeBuildInfo.sh - writing version $1"
   echo "instance.version=$1" > classes/BuildInfo.properties
   echo "versionDate_ddmmyyyy=$(date +%d/%m/%Y)" >> classes/BuildInfo.properties
+  echo "configuration.version=$1" > configurations/WebformulierenVerwerker/BuildInfo.properties
+  echo "configuration.timestamp=$(date +%Y%m%d-%H%M%S)" >> configurations/WebformulierenVerwerker/BuildInfo.properties
 else
   echo "writeBuildInfo.sh - no version to write, leaving BuildInfo.properties unchanged"
 fi
